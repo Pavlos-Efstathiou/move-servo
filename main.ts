4763321821824239607af37447aa08984f6d5ec2
Pavlos Efstathiou <Jimosmekma@gmail.com>
2020-12-29 14:59:30 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,6 +21,9 @@ while (true) {
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 180)
     basic.pause(200)
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
+    basic.pause(200)
 }
 basic.forever(function () {
 	
